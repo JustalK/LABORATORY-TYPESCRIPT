aaae54d767f7da279153c5bb4bb7da1f8450feeb
justalk <justal.kevin@gmail.com>
2021-10-17 10:45:54 +0200

    [TASK-7][FEATURE] Adding default props

---
+++ src/pages/Experience2/index.tsx
@@ -0,0 +1,24 @@
+/**
+ * The module managing the home page
+ * @module Home
+ */
+
+import React from 'react'
+
+type ExperienceProps = {
+  name: string,
+  count?: number
+}
+
+/**
+ * @function Home
+ * Create the component Home
+ * @return {Object} Return the dom of the Home page
+ */
+const Experience = ({ name, count }: ExperienceProps): JSX.Element => <>Test</>
+
+Experience.defaultProps = {
+  count: 0
+}
+
+export default Experience
